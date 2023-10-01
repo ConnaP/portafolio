2369833f092c963a317af1829f2d75c9ccb050e3
coni <coni.palavecino@gmail.com>
2023-09-30 23:41:42 -0300

    Se corrige codigo

--- src/components/Animateds/AnimatedSkillsComponent.jsx
+++ src/components/Animateds/AnimatedSkillsComponent.jsx
@@ -1,6 +1,7 @@
 "use client";
 import { motion } from "framer-motion";
 import "@/components/Css/AnimatedSkillsComponent.css";
+import Image from "next/image";
 
 const AnimatedSkillsComponent = () => {
   const skillsImage = [
@@ -35,8 +36,9 @@ const AnimatedSkillsComponent = () => {
 
   return (
     <div className="flex flex-wrap container-p items-center justify-center min-h-screen from-teal-100 via-teal-300 to-teal-500">
-      {skillsImage.map((img) => (
+      {skillsImage.map((img, index) => (
         <motion.div
+          key={index}
           className="flex-initial w-52 h-52"
           initial="initial"
           animate="animate"
@@ -44,7 +46,7 @@ const AnimatedSkillsComponent = () => {
         >
           <a className="flex" href="#">
             <div className="overflow-hidden aspect-video cursor-pointer rounded-xl relative group w-52 h-52 container-image">
-              <img
+              <Image
                 alt=""
                 className="object-cover w-full aspect-square group-hover:scale-110 transition duration-300 ease-in-out img-skill"
                 src={img}
