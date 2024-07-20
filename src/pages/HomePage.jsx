9247d522777d9dc2c75c198d84c678519358c69c
coni <coni.palavecino@gmail.com>
2024-07-19 22:12:17 -0400

    ux/ui

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,7 +6,7 @@ import '@/pages/css/home.css'
 
 const HomePage = () => {
   return (
-    <div className="w-full h-full pl-6 pr-20">
+    <div className="w-full h-full px-24">
       <main className="flex items-center justify-between w-full container-home">
         <AnimatedImageComponent
           heightIamge={600}
